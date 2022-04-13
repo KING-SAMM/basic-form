9e2a6efdafcdee02be1e39378815e2baef0afb97
KING-Samm <kcsamm11@gmail.com>
2022-04-13 23:56:43 +0100

    Added state change functionality to firstname

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 
 const App = () => 
 {
+  const [ values, useValues ] = useState({
+    firstname: '',
+    lastname: '',
+    email: ''
+  });
+
+  const handleFirstnameInoutChange = (e) => 
+  {
+    setValues({ ...values, firstname: e.target.value });
+  }
 
   return (
     <div className="App">
@@ -10,18 +20,22 @@ const App = () =>
           className="form-field"
           name="firstname" 
           placeholder="First Name"
+          value={ values.firstname }
+          onChange={ handleFirstnameInoutChange }
         /> <br />
         <input 
           type="text" 
           className="form-field"
           name="lastname" 
           placeholder="Last Name"
+          value={ values.lastname }
         /> <br />
         <input 
           type="email" 
           className="form-field"
           name="email" 
           placeholder="Email"
+          value={ values.email }
         /> <br />
         <input 
           type="submit" 
