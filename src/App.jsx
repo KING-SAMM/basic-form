6a503bd45c318ec663454c3e8e643de19e5fcfa4
KING-Samm <kcsamm11@gmail.com>
2022-04-14 19:32:27 +0100

    Implemented submit handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,55 @@
+import { useState } from 'react';
 
 const App = () => 
 {
-  const [ values, useValues ] = useState({
+  const [ values, setValues ] = useState({
     firstname: '',
     lastname: '',
     email: ''
   });
 
-  const handleFirstnameInoutChange = (e) => 
+  const [ submitted, setSubmitted ] = useState(false);
+
+  const handleFirstnameInputChange = (e) => 
   {
     setValues({ ...values, firstname: e.target.value });
+  };
+
+  const handleLastnameInputChange = (e) => 
+  {
+    setValues({ ...values, lastname: e.target.value });
+  };
+
+  const handleEmailInputChange = (e) => 
+  {
+    setValues({ ...values, email: e.target.value });
+  };
+
+  const handleSubmit = (e) => 
+  {
+    e.preventDefault();
+
+    setSubmitted(true);
   }
 
   return (
     <div className="App">
-      <form className="reg-form" action="">
+      <h2>Inputs</h2>
+      <h3>{ values.firstname }</h3>
+      <h3>{ values.lastname }</h3>
+      <h3>{ values.email }</h3>
+      <form className="reg-form" onSubmit={ handleSubmit }>
+        { submitted 
+        ? <h3 className="success-message">Registration successful!</h3> 
+        : null
+        };
         <input 
           type="text" 
           className="form-field"
           name="firstname" 
           placeholder="First Name"
           value={ values.firstname }
-          onChange={ handleFirstnameInoutChange }
+          onChange={ handleFirstnameInputChange }
         /> <br />
         <input 
           type="text" 
@@ -29,6 +57,7 @@ const App = () =>
           name="lastname" 
           placeholder="Last Name"
           value={ values.lastname }
+          onChange={ handleLastnameInputChange }
         /> <br />
         <input 
           type="email" 
@@ -36,6 +65,7 @@ const App = () =>
           name="email" 
           placeholder="Email"
           value={ values.email }
+          onChange={ handleEmailInputChange }
         /> <br />
         <input 
           type="submit" 
